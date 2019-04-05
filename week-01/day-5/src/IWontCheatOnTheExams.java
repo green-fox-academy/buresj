977eb18ff5c285b2dc92ec11c91a9858d8cf8900
Jiri Bures <bures@fakultnisociologie.cz>
2019-04-05 11:09:58 +0200

    finished IWontCheatOnTheExams exercise

---
+++ week-01/day-5/src/IWontCheatOnTheExams.java
@@ -0,0 +1,12 @@
+// Create a program that writes this line 100 times:
+// "I won't cheat on the exam!"
+
+public class IWontCheatOnTheExams {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 100 ; i++) {
+            System.out.println("I won't cheat on the exam!");
+        }
+
+    }
+}
