1dca3339ffe3a6041e361a0bf9d640d0b1ea6bb2
Jiri Bures <bures@fakultnisociologie.cz>
2019-04-05 11:18:09 +0200

    finished PrintEven

---
+++ week-01/day-5/src/PrintEven.java
@@ -0,0 +1,14 @@
+
+
+public class PrintEven {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i <= 500 ; i +=2) {
+
+            System.out.println(i);
+        }
+
+    }
+
+}
