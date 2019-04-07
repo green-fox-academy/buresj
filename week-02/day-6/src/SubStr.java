52016507532a83880128fbabf358b8480758f121
Jiri Bures <bures@fakultnisociologie.cz>
2019-04-07 22:57:03 +0200

    init day-6 folder and moving SubStr.java to it

---
+++ week-02/day-6/src/SubStr.java
@@ -0,0 +1,11 @@
+//  Create a function that takes two strings as a parameter
+//  Returns the starting index where the second one is starting in the first one
+//  Returns `-1` if the second string is not in the first one
+
+public class SubStr {
+
+    public static void main(String[] args) {
+
+    }
+
+}
