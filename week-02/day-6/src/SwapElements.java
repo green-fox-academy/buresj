d09703d61a76786728d872ed3673eff8fd8c9b2b
Jiri Bures <bures@fakultnisociologie.cz>
2019-04-08 14:47:00 +0200

    SwapElements

--- week-02/day-6/src/SwapElements.java
+++ week-02/day-6/src/SwapElements.java
@@ -6,5 +6,13 @@ public class SwapElements {
     public static void main(String[] args) {
 
         String[] abc = {"first", "second", "third"};
+
+        String d = "";
+        d = abc[0];
+        abc[0] = abc[2];
+        abc[2] = d;
+
+        System.out.println(abc[2]);
+
     }
 }
