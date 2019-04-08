231f1fb908f958818131fc33ab839272046eb0fa
Jiri Bures <bures@fakultnisociologie.cz>
2019-04-08 16:48:39 +0200

    finished SubInt

--- week-02/day-6/src/SubInt.java
+++ week-02/day-6/src/SubInt.java
@@ -6,5 +6,31 @@ public class SubInt {
 
     public static void main(String[] args) {
 
+        int[] array = {1,2,4,4,5,6};
+        int num = 4;
+
+        int[] results = finder(array, num);
+
+        for (int i = 0; i < results.length; i++) {
+
+            System.out.print(results[i] + ", ");
+        }
+
     }
+    
+    public static int[] finder (int[] testedArray, int testedNum) {
+
+        int[] results = new int[testedArray.length];
+        int j = 0;
+
+        for (int i = 0; i < testedArray.length; i++) {
+
+            if (testedArray[i] == testedNum) {
+                results[j] = i;
+                j++;
+            }
+        }
+        return results;
+    }
+
 }
