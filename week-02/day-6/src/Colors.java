dab8075c58b2b65cd1a8d98db3e027a4dd17bfbd
Jiri Bures <bures@fakultnisociologie.cz>
2019-04-08 14:38:12 +0200

    finished Colors

--- week-02/day-6/src/Colors.java
+++ week-02/day-6/src/Colors.java
@@ -7,10 +7,35 @@
 // - In `colors[2]` store the shades of pink:
 //   `"orchid", "violet", "pink", "hot pink"`
 //
-//public class Colors {
-//    public static void main(String[] args) {
-//
-//        String colors[][] = new String[][];
-//
-//    }
-//}
+
+public class Colors {
+    public static void main(String[] args) {
+
+//        String shadesOfGreen[] = {"lime", "forest green", "olive", "pale green", "spring green"};
+//        String shadesOfRed[] = {"orange red", "red", "tomato"};
+//        String shadesOfPink[] = {"orchid", "violet", "pink", "hot pink"};
+
+        String colors[][] = new String[2][5];
+
+        colors[0][0] = "green";
+        colors[1][0] = "red";
+        colors[2][0] = "pink";
+
+        colors[0][1] = "lime";
+        colors[0][2] = "fores green";
+        colors[0][3] = "olive";
+        colors[0][4] = "pale green";
+        colors[0][5] = "spring";
+
+        colors[1][1] = "orange red";
+        colors[1][2] = "red";
+        colors[1][3] = "tomato";
+
+        colors[2][1] = "orchid";
+        colors[2][2] = "violet";
+        colors[2][3] = "pink";
+        colors[2][4] = "hot pink";
+
+
+    }
+}
---
+++ week-02/day-6/src/SwapElements.java
@@ -0,0 +1,10 @@
+// - Create an array variable named `abc`
+//   with the following content: `["first", "second", "third"]`
+// - Swap the first and the third element of `abc`
+
+public class SwapElements {
+    public static void main(String[] args) {
+
+        String[] abc = {"first", "second", "third"};
+    }
+}
