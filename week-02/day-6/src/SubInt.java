926b7b8869d558c29be1d4de3c3bba8ee8204b29
Jiri Bures <bures@fakultnisociologie.cz>
2019-04-08 15:47:32 +0200

    finished Reverse

---
+++ week-02/day-6/src/SubInt.java
@@ -0,0 +1,10 @@
+//  Create a function that takes a number and an array of integers as a parameter
+//  Returns the indices of the integers in the array of which the first number is a part of
+//  Or returns an empty array if the number is not part of any of the integers in the array
+
+public class SubInt {
+
+    public static void main(String[] args) {
+
+    }
+}
--- week-02/day-6/src/SumAll.java
+++ week-02/day-6/src/SumAll.java
@@ -7,7 +7,6 @@ public class SumAll {
     public static void main(String[] args) {
 
         int sum = 0;
-
         int[] ai = {3, 4, 5, 6, 7};
 
         for (int i = 0; i < ai.length; i++) {
