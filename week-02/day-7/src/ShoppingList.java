fe3b8eeea26b8d75e8c67dbe75126c475e86904f
Jiri Bures <bures@fakultnisociologie.cz>
2019-04-09 17:07:51 +0200

    ShoppingList finished

---
+++ week-02/day-7/src/ShoppingList.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+
+        ArrayList<String> shopList = new ArrayList<>(Arrays.asList("eggs", "milk", "fish", "apples", "bread", "chicken"));
+
+        isItInList("milk",shopList);
+    }
+
+    public static void isItInList (String input, ArrayList<String> inputList){
+
+        boolean checker = false;
+
+        for (String item:
+             inputList) {
+            if (item == input) {
+                checker = true;
+            }
+        }
+
+        if (checker) {
+            System.out.println("It is there!");
+        }else{
+            System.out.println("It is not there!");
+        }
+    }
+}
