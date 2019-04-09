9711ed28fda03c67c3866e8dbd29107aeec4e070
Jiri Bures <bures@fakultnisociologie.cz>
2019-04-09 16:52:26 +0200

    finished TelephoneBook

---
+++ week-02/day-7/src/TelephoneBook.java
@@ -0,0 +1,34 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class TelephoneBook {
+    public static void main(String[] args) {
+
+        HashMap<String, String> book = new HashMap<>();
+
+        book.put("William A. Lathan", "405-709-1865");
+        book.put("John K. Miller", "402-247-8568");
+        book.put("Hortensia E. Foster", "606-481-6467");
+        book.put("Amanda D. Newland", "319-243-5613");
+        book.put("Brooke P. Askew", "307-687-2982");
+
+        System.out.println(book.get("John K. Miller"));
+
+        boolean chris = false;
+
+        for (Map.Entry<String, String> name : book.entrySet()) {
+
+            if (name.getValue() == "Chris E. Myers") {
+                chris = true;
+            }else{
+                chris = false;
+            }
+        }
+
+        if(chris) {
+            System.out.println("He is here.");
+        }else {
+            System.out.println("He is not here.");
+        }
+    }
+}
