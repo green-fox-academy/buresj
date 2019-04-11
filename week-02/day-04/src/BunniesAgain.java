eb6ec1ea67007624eedbc7faa50b350d74a25c19
Jiri Bures <bures@fakultnisociologie.cz>
2019-04-11 14:44:30 +0200

    finished BunniesAgain

--- week-02/day-04/src/BunniesAgain.java
+++ week-02/day-04/src/BunniesAgain.java
@@ -1,11 +1,23 @@
 //We have bunnies standing in a line, numbered 1, 2, ... The odd bunnies (1, 3, ..) have the normal 2 ears.
-//        The even bunnies (2, 4, ..) we'll say have 3 ears, because they each have a raised foot.
-//        Recursively return the number of "ears" in the bunny line 1, 2, ... n (without loops or multiplication).
+//The even bunnies (2, 4, ..) we'll say have 3 ears, because they each have a raised foot.
+//Recursively return the number of "ears" in the bunny line 1, 2, ... n (without loops or multiplication).
 
 public class BunniesAgain {
     public static void main(String[] args) {
 
+        System.out.println(bunnies(5));
     }
 
-    public static int bunnies ()
+    public static int bunnies(int n) {
+
+        if (n == 1) {
+            return 3;
+        } else {
+            if (n % 2 == 0) {
+                return 2 + bunnies(n - 1);
+            } else {
+                return 3 + bunnies(n - 1);
+            }
+        }
+    }
 }
--- week-02/day-04/src/CommonDivisor.java
+++ week-02/day-04/src/CommonDivisor.java
@@ -8,10 +8,10 @@ public class CommonDivisor {
 
     public static int commonD(int n, int m, int divisor) {
 
-        if (n%divisor==0 && m%divisor==0 ) {
+        if (n % divisor == 0 && m % divisor == 0) {
             return divisor;
         } else {
-            return commonD(n,m, divisor - 1);
+            return commonD(n, m, divisor - 1);
         }
     }
 }
