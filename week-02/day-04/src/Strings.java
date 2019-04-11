43994fa63704a3888b524cbdc9c61fcae1a4be61
Jiri Bures <bures@fakultnisociologie.cz>
2019-04-11 15:46:53 +0200

    finished Strings

---
+++ week-02/day-04/src/Strings.java
@@ -0,0 +1,25 @@
+//Given a string, compute recursively (no loops) a new string where all the lowercase 'x' chars have been changed to 'y' chars.
+
+public class Strings {
+
+    public static void main(String[] args) {
+
+        System.out.println(changeToY("xfdxxfd f afdafx"));
+    }
+
+
+    public static String changeToY(String text) {
+
+        int positionX;
+        positionX = text.indexOf("x");
+        char[] letters = text.toCharArray();
+
+        if (positionX == -1) {
+            return text;
+        } else {
+            letters[positionX] = 'y';
+            String xtext = new String(letters);
+            return changeToY(xtext);
+        }
+    }
+}
