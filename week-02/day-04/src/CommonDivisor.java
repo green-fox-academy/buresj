c2970ae78cfe699a1f49f7934eb3a691ce90555c
Jiri Bures <bures@fakultnisociologie.cz>
2019-04-12 08:45:43 +0200

    alterantive solution to CommonDivisor and unsolved Fibonacci

--- week-02/day-04/src/CommonDivisor.java
+++ week-02/day-04/src/CommonDivisor.java
@@ -2,16 +2,27 @@
 public class CommonDivisor {
     public static void main(String[] args) {
 
-        System.out.println(commonD(45, 60, 100));
+        System.out.println(commonD(45, 60));
         //15
     }
 
-    public static int commonD(int n, int m, int divisor) {
+//    public static int commonD(int n, int m, int divisor) {
+//
+//        if (n % divisor == 0 && m % divisor == 0) {
+//            return divisor;
+//        } else {
+//            return commonD(n, m, divisor - 1);
+//        }
+//    }
+
+    public static int commonD(int n, int m) {
+
+        int divisor = (int) (Math.random() * m+n + 1);
 
         if (n % divisor == 0 && m % divisor == 0) {
             return divisor;
         } else {
-            return commonD(n, m, divisor - 1);
+            return commonD(n, m);
         }
     }
 }
