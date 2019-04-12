cead67bfa6fb25e86a93bbc7297fae9919aec2dc
Jiri Bures <bures@fakultnisociologie.cz>
2019-04-12 12:56:36 +0200

    cleaning code in solutions for CommonDivisor

--- week-02/day-04/src/CommonDivisor.java
+++ week-02/day-04/src/CommonDivisor.java
@@ -2,27 +2,30 @@
 public class CommonDivisor {
     public static void main(String[] args) {
 
-        System.out.println(commonD(45, 60));
+        System.out.println(commonD(45, 60, 100));
+        //15
+
+        System.out.println(commonDRandom(45, 60));
         //15
     }
 
-//    public static int commonD(int n, int m, int divisor) {
-//
-//        if (n % divisor == 0 && m % divisor == 0) {
-//            return divisor;
-//        } else {
-//            return commonD(n, m, divisor - 1);
-//        }
-//    }
-
-    public static int commonD(int n, int m) {
-
-        int divisor = (int) (Math.random() * m+n + 1);
+    public static int commonD(int n, int m, int divisor) {
 
         if (n % divisor == 0 && m % divisor == 0) {
             return divisor;
         } else {
-            return commonD(n, m);
+            return commonD(n, m, divisor - 1);
+        }
+    }
+
+    public static int commonDRandom(int n, int m) {
+
+        int divisor = (int) (Math.random() * 100 + 1);
+
+        if (n % divisor == 0 && m % divisor == 0) {
+            return divisor;
+        } else {
+            return commonDRandom(n, m);
         }
     }
 }
