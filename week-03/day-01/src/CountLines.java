97667820a52acd554eed312db89af01a8d5b7504
Jiri Bures <bures@fakultnisociologie.cz>
2019-04-15 13:28:15 +0200

    finished CountLine

---
+++ week-03/day-01/src/CountLines.java
@@ -0,0 +1,24 @@
+// Write a function that takes a filename as string,
+// then returns the number of lines the file contains.
+// It should return zero if it can't open the file, and
+// should not raise any error.
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class CountLines {
+    public static void main(String[] args) {
+
+        try {
+        Path filePath = Paths.get("assets/lorem.txt");
+        List<String> lines = Files.readAllLines(filePath);
+
+        System.out.println(lines.size());
+    
+        } catch (Exception e) {
+            System.out.println("File is not accessible");
+        }
+    }
+}
