7869fd1e849cc6bf661e0794a8808e2745d3fd5d
Jiri Bures <bures@fakultnisociologie.cz>
2019-04-15 17:31:38 +0200

    files and setup for TicTacToe task

--- week-03/day-01/src/TicTacToe.java
+++ week-03/day-01/src/TicTacToe.java
@@ -3,27 +3,38 @@
 //// We have provided you some example files (draw.txt, win-x.txt, win-o.txt)
 //// Return "X", "O" or "Draw" based on the input file
 //
+//import java.nio.file.Files;
 //import java.nio.file.Path;
 //import java.nio.file.Paths;
+//import java.util.ArrayList;
+//import java.util.List;
 //
 //public class TicTacToe {
 //
 //    public static void main(String[] args) {
 //
 //
-//        System.out.println(ticTacResult("win-o.txt"))
+//        System.out.println(ticTacResult("win-o.txt"));
 //        // Should print "O"
 //
-//        System.out.println(ticTacResult("win-x.txt"))
+//        System.out.println(ticTacResult("win-x.txt"));
 //        // Should print "X"
 //
-//        System.out.println(ticTacResult("draw.txt"))
+//        System.out.println(ticTacResult("draw.txt"));
 //        // Should print "Draw"
 //    }
 //
 //
 //    public static String ticTacResult (String name) {
 //
-//        Path pathOne = Paths.get("assets/" + logFile);
+//        Path path = Paths.get("assets/" + name);
+//
+//        List<String> tic = new ArrayList<>();
+//
+//        try {
+//            tic = Files.readAllLines(path);
+//        } catch (Exception e) {
+//            System.out.println("Unable to reach a file.");
+//        }
 //    }
 //}
