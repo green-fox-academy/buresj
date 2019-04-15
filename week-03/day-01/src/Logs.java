50ffd1c5b8c6b8dfd758f4b6acf14744d14cb7dd
Jiri Bures <bures@fakultnisociologie.cz>
2019-04-15 17:00:36 +0200

    finished log.txt

--- week-03/day-01/src/Logs.java
+++ week-03/day-01/src/Logs.java
@@ -1,4 +1,64 @@
-package PACKAGE_NAME;
+// Read all data from 'log.txt'.
+// Each line represents a log message from a web server
+// Write a function that returns an array with the unique IP adresses.
+// Write a function that returns the GET / POST request ratio.
+
+import java.lang.reflect.Array;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Logs {
+    public static void main(String[] args) {
+
+        System.out.println(collectsIPs("log.txt"));
+        System.out.println(printRequestRatio("log.txt"));
+    }
+
+    public static List<String> collectsIPs(String logFile) {
+
+        Path pathOne = Paths.get("assets/" + logFile);
+        List<String> lines = new ArrayList<>();
+        List<String> ipList = new ArrayList<>();
+
+        try {
+            lines = Files.readAllLines(pathOne);
+        } catch (Exception e) {
+            System.out.println("Unable to reach a file.");
+        }
+
+        for (String line : lines) {
+            ipList.add(line.substring(27, 38));
+        }
+
+        return ipList;
+    }
+
+    public static float printRequestRatio(String logFile) {
+
+        int counter = 0;
+
+        Path pathOne = Paths.get("assets/" + logFile);
+        List<String> lines = new ArrayList<>();
+        List<String> requestList = new ArrayList<>();
+
+        try {
+            lines = Files.readAllLines(pathOne);
+        } catch (Exception e) {
+            System.out.println("Unable to reach a file.");
+        }
+
+        for (String line : lines) {
+            requestList.add(line.substring(41, 44));
+        }
+
+        for (String line : requestList) {
+            if (line.contains("GET")) {
+                counter++;
+            }
+        }
+        return counter;
+    }
 }
