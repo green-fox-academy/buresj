ea4e1d2d3e0f8d32e69d0216450bb6863e3634e7
Jiri Bures <bures@fakultnisociologie.cz>
2019-04-15 10:07:41 +0200

    finished Fibonacci and basic recursion for SquaresInSquares

--- week-02/day-04/src/Fibonacci.java
+++ week-02/day-04/src/Fibonacci.java
@@ -1,16 +1,31 @@
 //0, 1, 1, 2, 3, 5, 8, 13, 21, 34
 
 public class Fibonacci {
+
     public static void main(String[] args) {
+
         System.out.println(fibIt(10));
     }
 
-    public static int fibIt (int n) {
+//    public static int fibIt(int n) {
+//
+//        if (n <= 1) {
+//            return n;
+//        } else {
+//            return fibIt(n - 1) + fibIt(n - 2);
+//        }
+//    }
 
-        if (n <= 0) {
-            return 1;
+    public static int fibIt(int n) {
+
+        System.out.println("------------------");
+
+        if (n <= 1) {
+            System.out.println("if result: " + n);
+            return n;
         } else {
-            return n + fibIt(fibIt(n - 1) - 1);
+            System.out.println("else result: " + n);
+            return (fibIt(n - 1)) + fibIt(n - 2);
         }
     }
 }
--- week-02/day-05/src/SquaresInSquares.java
+++ week-02/day-05/src/SquaresInSquares.java
@@ -13,28 +13,57 @@ public class SquaresInSquares {
 //        miniRect(graphics, 1, 2);
 //        miniRect(graphics, 2, 1);
 
-        miniRect(graphics, 10, 2,2);
+        // pyramid(graphics, 100, 10);
+
+        fourSquares(graphics, WIDTH, WIDTH, HEIGHT);
+
+//        for (int i = 10; i < 100; i += 2) {
+//
+//                graphics.drawRect(WIDTH / 2 - i,
+//                        HEIGHT / 2 - i,
+//                        i * 2,
+//                        i * 2);
+//            }
+//
     }
 
-    public static int miniRect(Graphics g, int size, int x, int y) {
+    public static void pyramid(Graphics g, int size, int diff) {
+
+        if (size >= 1) {
+            pyramid(g, size - diff, diff + 1);
+        }
 
         g.setColor(Color.black);
-        g.drawRect(WIDTH / x + size,
-                HEIGHT / y + size,
-                size * x,
-                size * y);
+        g.drawRect(WIDTH / 2 - size,
+                HEIGHT / 2 - size,
+                size * 2,
+                size * 2);
+    }
 
-        if (size == 1) {
-            return 1;
-        } else {
-            return miniRect(g, size - 1, x, y);
+    public static void fourSquares(Graphics g, int size, int x, int y) {
+
+        g.setColor(Color.black);
+        g.drawRect(WIDTH / 3 - size,
+                HEIGHT / 3 - size,
+                size,
+                size);
+
+        if (size >= 1) {
+
+            fourSquares(g, size / 3, x - size / 3, 0);
+            fourSquares(g, size / 3, x - size / 3, 0);
+
+//            fourSquares(g, size / 3, 0, x - size / 3);
+//            fourSquares(g, size / 3, x * 2 / 3 + size / 3, y / 3 + size / 3);
+//            fourSquares(g, size / 3, x / 3 + size / 3, y * 2 / 3 + size / 3);
+
         }
     }
 
 
     // Don't touch the code below
-    static int WIDTH = 320;
-    static int HEIGHT = 320;
+    static int WIDTH = 330;
+    static int HEIGHT = 330;
 
     public static void main(String[] args) {
         JFrame jFrame = new JFrame("Drawing");
