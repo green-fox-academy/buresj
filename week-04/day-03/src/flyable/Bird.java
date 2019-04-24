bb5c67a9e538f0f19c068132812fc02566b13f59
Jiri Bures <bures@fakultnisociologie.cz>
2019-04-24 15:40:46 +0200

    in Flyable Bird class finished

---
+++ week-04/day-03/src/flyable/Bird.java
@@ -0,0 +1,24 @@
+package flyable;
+import main.java.animals.*;
+
+public class Bird extends main.java.animals.Bird implements Flyable {
+
+    public Bird(String name) {
+        super(name);
+    }
+
+    @Override
+    public void land() {
+
+    }
+
+    @Override
+    public void fly() {
+
+    }
+
+    @Override
+    public void takeOff() {
+
+    }
+}
