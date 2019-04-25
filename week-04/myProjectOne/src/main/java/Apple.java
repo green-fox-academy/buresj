3fbec9163b9d600bc772c89bb836e91a2ced0016
Jiri Bures <bures@fakultnisociologie.cz>
2019-04-25 11:43:54 +0200

    finished Apple

---
+++ week-04/myProjectOne/src/main/java/Apple.java
@@ -0,0 +1,8 @@
+public class Apples {
+
+    public String getApple() {
+        String apple = "apple";
+        return apple;
+    }
+
+}
---
+++ week-04/myProjectOne/src/test/java/AppleTest.java
@@ -0,0 +1,6 @@
+public class ApplesTest {
+
+
+public void AppleTest_initApples
+
+}
