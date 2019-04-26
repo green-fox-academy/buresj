2b84aa9295759e72fd9da2417cf7402351c71399
Jiri Bures <bures@fakultnisociologie.cz>
2019-04-26 12:47:39 +0200

    TodoApp: finished CLI

---
+++ week-04/day-05/src/main/java/CLI.java
@@ -0,0 +1,11 @@
+public class CLI {
+
+    public void printMenu() {
+        System.out.println("Command Line Todo application");
+        System.out.println("=============================");
+        System.out.println("-l   Lists all the tasks");
+        System.out.println("-a   Adds a new task");
+        System.out.println("-r   Removes a task");
+        System.out.println("-c   Completes a task");
+    }
+}
--- week-04/day-05/src/main/java/TodoApp.java
+++ week-04/day-05/src/main/java/TodoApp.java
@@ -2,11 +2,7 @@ public class TodoApp {
 
     public static void main(String[] args) {
 
-            System.out.println("Command Line Todo application");
-            System.out.println("=============================");
-            System.out.println("-l   Lists all the tasks");
-            System.out.println("-a   Adds a new task");
-            System.out.println("-r   Removes a task");
-            System.out.println("-c   Completes a task");
+          CLI cli = new CLI();
+          cli.printMenu();
     }
 }
