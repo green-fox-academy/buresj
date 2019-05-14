59b5766e1ca5e20f3a346daf7ffe49fe82f6d85b
Jiri Bures <bures@fakultnisociologie.cz>
2019-05-14 17:03:21 +0200

    MostCommonWords finished

---
+++ week-06/day-02/src/mostcommonwords/MostCommonWords.java
@@ -0,0 +1,36 @@
+package mostcommonwords;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class MostCommonWords {
+
+    public static void main(String[] args) {
+
+        List<String> lines = new ArrayList<>();
+        Path filePath = Paths.get("assets/wikiArticle.txt");
+
+        try {
+            lines = Files.readAllLines(filePath);
+        } catch (Exception e) {
+            System.out.println("File is not accessible");
+        }
+
+
+        Map<String, Long> counter = lines.stream()
+                .flatMap(line -> Arrays.stream(line.trim().split(" ")))
+                .map(word -> word.replaceAll("[^a-zA-Z]", "").toLowerCase().trim())
+                .filter(word -> !word.isEmpty())
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
+
+        counter.entrySet().stream()
+                .limit(100)
+                .sorted(Comparator.comparing(Map.Entry::getValue))
+                .forEach(e -> System.out.println(e.getKey() + ": " + e.getValue()));
+    }
+}
