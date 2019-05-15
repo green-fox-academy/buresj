8ac1ca7065a1038f9853ffc568e1eb3cd5a01bdc
Jiri Bures <bures@fakultnisociologie.cz>
2019-05-15 16:28:32 +0200

    Say hello to all world

--- week-06/day-03/src/main/java/com/greenfoxacademy/springstart/controllers/HelloToAllWorld.java
+++ week-06/day-03/src/main/java/com/greenfoxacademy/springstart/controllers/HelloToAllWorld.java
@@ -1,4 +1,3 @@
-/*
 package com.greenfoxacademy.springstart.controllers;
 
 import org.springframework.stereotype.Controller;
@@ -6,21 +5,24 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.Random;
 import java.util.concurrent.atomic.AtomicLong;
 
 @Controller
-public class HelloWebController {
+public class HelloToAllWorld {
 
     AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/web/greeting")
     public String greeting(@RequestParam String name, Model model) {
         Greeting greets = new Greeting(counter.incrementAndGet(), name);
+        ProduceHello hello = new ProduceHello();
 
-        model.addAttribute("id", greets.getId());
+        model.addAttribute("color", hello.getColor());
+        model.addAttribute("size", hello.getSize());
+        model.addAttribute("hello", hello.getHellos(counter.intValue()));
         model.addAttribute("name", name);
 
         return "greeting";
     }
 }
-*/
