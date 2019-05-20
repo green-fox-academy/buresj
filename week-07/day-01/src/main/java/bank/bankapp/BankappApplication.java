aaea365cd4b78bd2c5eb841346dc849b92b6b770
Jiri Bures <bures@fakultnisociologie.cz>
2019-05-20 10:24:46 +0200

    BankApp: project established

---
+++ week-07/day-01/src/main/java/bank/bankapp/BankappApplication.java
@@ -0,0 +1,13 @@
+package bank.bankapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankappApplication.class, args);
+    }
+
+}
