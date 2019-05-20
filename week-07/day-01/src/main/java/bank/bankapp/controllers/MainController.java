4dbfcd025dbbfaa1517eb22469b04b8ce98f2ce5
Jiri Bures <bures@fakultnisociologie.cz>
2019-05-20 21:23:07 +0200

    BankApp: finished

--- week-07/day-01/src/main/java/bank/bankapp/controllers/MainController.java
+++ week-07/day-01/src/main/java/bank/bankapp/controllers/MainController.java
@@ -5,22 +5,28 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Controller
 public class MainController {
 
-    List<BankAccount> accounts = Arrays.asList(
-            new BankAccount(1, "Simba", 2430.00d, "lion", "good"),
-            new BankAccount(2, "Scar", 975743.06d, "lion", "bad", true),
-            new BankAccount(3, "Pumba", 5453.00d, "warthog", "good"),
-            new BankAccount(4, "Timon", 766.00d, "meerkat ", "good"),
-            new BankAccount(5, "Rafiki", 1.43d, "baboon", "good"));
+    List<BankAccount> accounts;
+
+    public MainController() {
+        this.accounts = new ArrayList<>(Arrays.asList(
+                new BankAccount(1, "Simba", 2430.00d, "lion", "good"),
+                new BankAccount(2, "Scar", 975743.06d, "lion", "bad", true),
+                new BankAccount(3, "Pumba", 5453.00d, "warthog", "good"),
+                new BankAccount(4, "Timon", 766.00d, "meerkat ", "good"),
+                new BankAccount(5, "Rafiki", 1.43d, "baboon", "good")));
+    }
 
     @RequestMapping(path = "/show", method = RequestMethod.GET)
     public String show(Model model) {
         model.addAttribute("accounts", accounts);
+        model.addAttribute("newAccount", new BankAccount());
         return "index";
     }
 
@@ -28,18 +34,15 @@ public class MainController {
     public String HTMLception(Model model) {
         String text = "This is an <em>HTML</em> text. <b>Enjoy yourself!</b>";
         model.addAttribute("text", text);
+        model.addAttribute("newAccount", new BankAccount());
         return "inception";
     }
 
-    @GetMapping("/add")
-    public String greetingForm(Model model) {
-        model.addAttribute("newAccount", new BankAccount());
-        return "index";
-    }
-
     @PostMapping("/add")
-    public String greetingSubmit(@ModelAttribute BankAccount account) {
+    public String addAcc(@ModelAttribute BankAccount account, Model model) {
+        model.addAttribute("newAccount", account);
         accounts.add(account);
+        model.addAttribute("accounts", accounts);
         return "index";
     }
 }
