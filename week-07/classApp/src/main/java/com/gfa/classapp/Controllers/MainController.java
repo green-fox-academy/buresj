f61246f35f3257a0bb69202aae4c3d4767171108
Jiri Bures <bures@fakultnisociologie.cz>
2019-05-22 15:23:01 +0200

    ClassApp: finished

--- week-07/classApp/src/main/java/com/gfa/classapp/Controllers/MainController.java
+++ week-07/classApp/src/main/java/com/gfa/classapp/Controllers/MainController.java
@@ -1,38 +1,48 @@
 package com.gfa.classapp.Controllers;
 
-import com.gfa.classapp.Services.StudentService;
+import com.gfa.classapp.Services.StudentServiceProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
 
-   private StudentService studentService;
+    private StudentServiceProvider provider1;
+    private StudentServiceProvider provider2;
 
     @Autowired
-    public MainController(StudentService studentService){
-        this.studentService = studentService;
+    public MainController(@Qualifier("list") StudentServiceProvider provider1,
+                          @Qualifier("IO") StudentServiceProvider provider2){
+        this.provider1= provider1;
+        this.provider2= provider2;
+
     }
 
     @RequestMapping("/gfa")
-    public String show() {
+    public String show(Model model) {
+        model.addAttribute("count",provider2.count());
         return "index";
     }
 
     @RequestMapping("/gfa/list")
     public String list(Model model) {
-        model.addAttribute("students", studentService.findAll());
+        model.addAttribute("students", provider2.findAll());
         return "list";
     }
 
     @RequestMapping("/gfa/add")
     public String addBookForm(@RequestParam(required = false) String name) {
-        studentService.save(name);
+        provider2.save(name);
         return "form";
+    }
+
+    @RequestMapping("/gfa/check")
+    public String checker(@RequestParam(required = false) String name, Model model) {
+        model.addAttribute("result", provider2.checker(name));
+        return "checker";
     }
 }
--- week-07/classApp/src/main/java/com/gfa/classapp/Services/StudentService.java
+++ week-07/classApp/src/main/java/com/gfa/classapp/Services/StudentService.java
@@ -1,10 +1,14 @@
 package com.gfa.classapp.Services;
+
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
+
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
-public class StudentService {
+@Qualifier("list")
+public class StudentService implements StudentServiceProvider {
     ArrayList<String> names;
 
     public StudentService() {
@@ -20,5 +24,20 @@ public class StudentService {
 
     public void save(String student) {
         names.add(student);
+    }
+
+    public String count() {
+        return String.valueOf(names.size());
+    }
+
+    public String checker(String name) {
+        String checker;
+        if (names.contains(name)) {
+            checker = "Student is in the list";
+        } else {
+            checker = "Student is not in the list";
+        }
+
+        return checker;
     }
 }
