4ae9a60fe386b56816d364231e5820dc46729c84
Jiri Bures <bures@fakultnisociologie.cz>
2019-05-23 11:15:52 +0200

    FoxClub: login corrected and working

--- week-07/day-04/src/main/java/com/foxes/foxapp/controllers/MainController.java
+++ week-07/day-04/src/main/java/com/foxes/foxapp/controllers/MainController.java
@@ -1,5 +1,6 @@
 package com.foxes.foxapp.controllers;
 
+import com.foxes.foxapp.models.Fox;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -7,24 +8,35 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 
 @Controller
 public class MainController {
 
+
+    List<Fox> foxes;
+
+    public MainController() {
+        this.foxes =  new ArrayList<>(Arrays.asList(new Fox("Foxi")));
+    }
+
     @RequestMapping(path = "/")
-    public String show(Model model) {
+    public String show(Model model, @RequestParam(required = false) String name) {
+        model.addAttribute("name", name);
         return "index";
     }
 
-
     @RequestMapping(path = "/login", method = RequestMethod.GET)
-    public String loginGet(@RequestParam(required = false) String name) {
-        return "redirect:/index" + name;
+    public String loginGet() {
+        return "login";
     }
 
     @RequestMapping(path = "/login", method = RequestMethod.POST)
     public String loginPost(@RequestParam(required = false) String name) {
-        return "redirect:/index" + name;
+        return "redirect:/?name=" + name;
     }
 
 }
