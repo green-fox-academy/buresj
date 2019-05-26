9e5282d044807bc7e0dd8b8438f3559c3771d3ad
Jiri Bures <bures@fakultnisociologie.cz>
2019-05-26 23:06:39 +0200

    FoxApp: basic RPG structure established

--- week-07/day-05/src/main/java/fox/app/controllers/MainController.java
+++ week-07/day-05/src/main/java/fox/app/controllers/MainController.java
@@ -1,4 +1,44 @@
 package fox.app.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
 public class MainController {
+
+    private final String URL = "?name=";
+    private String fox;
+
+    @GetMapping("/")
+    public String main(@RequestParam(required = false) String name, Model model) {
+        model.addAttribute("name", name);
+        return "index";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String handleLogin(@RequestParam String name) {
+        fox = name;
+        return "redirect:/" + URL + fox;
+    }
+
+    @GetMapping("/nav")
+    public String nutrition() {
+        return "redirect:/nutrition" + URL + fox;
+    }
+
+    @GetMapping("/nutrition")
+    public String nut(@RequestParam(required = false) String name, Model model) {
+        model.addAttribute("name", name);
+        return "nutrition";
+    }
+
 }
+
