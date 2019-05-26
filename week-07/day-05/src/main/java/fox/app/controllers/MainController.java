ddcc1edd28218f2ea88e18b2dcc991ccab162b9a
Jiri Bures <bures@fakultnisociologie.cz>
2019-05-26 23:55:05 +0200

    FoxApp: /login endpoints with redirectAttributes added

--- week-07/day-05/src/main/java/fox/app/controllers/MainController.java
+++ week-07/day-05/src/main/java/fox/app/controllers/MainController.java
@@ -5,12 +5,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class MainController {
-
-    private final String URL = "?name=";
-    private String fox;
 
     @GetMapping("/")
     public String main(@RequestParam(required = false) String name, Model model) {
@@ -24,21 +22,12 @@ public class MainController {
     }
 
     @PostMapping("/login")
-    public String handleLogin(@RequestParam String name) {
-        fox = name;
-        return "redirect:/" + URL + fox;
+    public String handleLogin(RedirectAttributes redirectAttributes, @RequestParam String name) {
+        redirectAttributes.addAttribute("name", name);
+        return "redirect:/";
     }
 
-    @GetMapping("/nav")
-    public String nutrition() {
-        return "redirect:/nutrition" + URL + fox;
-    }
 
-    @GetMapping("/nutrition")
-    public String nut(@RequestParam(required = false) String name, Model model) {
-        model.addAttribute("name", name);
-        return "nutrition";
-    }
 
 }
 
